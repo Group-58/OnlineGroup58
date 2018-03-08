3616648a0f0cab827f09a3651f1a3893d7fd28e0
Dimitar Vatev <div007@gmail.com>
2018-03-08 20:55:22 +0200

    Folder structure and add Problem 1

---
+++ intro-and-basic-syntax/exercise/Ptoblem 1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _01._Debit_Card_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = { 0, 0, 0, 0 };
+
+            for (int i = 0; i < 4; i++)
+            {
+                num[i] = int.Parse(Console.ReadLine()); 
+            }
+
+            Console.WriteLine($"{num[0]:D4} {num[1]:D4} {num[2]:D4} {num[3]:D4}");
+        }
+    }
+}
