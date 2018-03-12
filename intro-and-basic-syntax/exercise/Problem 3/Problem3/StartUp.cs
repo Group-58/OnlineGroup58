5dc284b4bdfa4111c0ba6f0f8e2c9c5d2fc95512
ryan8811 <todor_papanov@abv.bg>
2018-03-12 21:29:43 +0000

    Documents the code of solution of problem 3

--- intro-and-basic-syntax/exercise/Problem 3/Problem3/StartUp.cs
+++ intro-and-basic-syntax/exercise/Problem 3/Problem3/StartUp.cs
@@ -5,9 +5,12 @@
     public class StartUp
     {
         public static void Main()
-        {
+        { 
+            // We read miles from the console.
             double miles = double.Parse(Console.ReadLine());
+            // After this we set how much is 1 mile to kilometers.
             double kilometers = miles * 1.60934;
+            // We print the result.
             Console.WriteLine($"{kilometers:f2}");
         }
     }
