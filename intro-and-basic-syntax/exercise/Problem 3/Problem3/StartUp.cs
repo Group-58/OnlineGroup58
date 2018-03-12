77f53449ec533413d791afed5baaac4f17d94b29
Dimitar Vatev <div007@gmail.com>
2018-03-12 14:02:01 +0200

    Add all files

---
+++ intro-and-basic-syntax/exercise/Problem 3/Problem3/StartUp.cs
@@ -0,0 +1,14 @@
+﻿namespace Problem3
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            double miles = double.Parse(Console.ReadLine());
+            double kilometers = miles * 1.60934;
+            Console.WriteLine($"{kilometers:f2}");
+        }
+    }
+}
