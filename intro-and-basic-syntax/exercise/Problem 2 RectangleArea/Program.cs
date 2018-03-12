8eb8bae3d93071aac553dd23430ba175f978d3b6
Dimitar Vatev <div007@gmail.com>
2018-03-12 13:56:16 +0200

    Document the code of the Problem 2

--- intro-and-basic-syntax/exercise/Problem 2 RectangleArea/Program.cs
+++ intro-and-basic-syntax/exercise/Problem 2 RectangleArea/Program.cs
@@ -10,10 +10,14 @@ namespace RectangleArea
     {
         static void Main(string[] args)
         {
+            // In the next two rows, we get the length of the sides "a" and "b" of the rectangle from the console
             double a = double.Parse(Console.ReadLine());
             double b = double.Parse(Console.ReadLine());
+
+            // We calculate area of rectangle.
             var sum = a * b;
 
+            // We print to the console the result rounded up to two marks after the decimal point.
             Console.WriteLine("{0:f2}",sum);
            
         }
