5f084ba9025228f8949989804d1d8a5fbd0dbe1f
Dimitar Vatev <div007@gmail.com>
2018-03-12 15:12:27 +0200

    Change a variable name in Problem 2

--- intro-and-basic-syntax/exercise/Problem 2 RectangleArea/Program.cs
+++ intro-and-basic-syntax/exercise/Problem 2 RectangleArea/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RectangleArea
 {
@@ -11,14 +7,14 @@ namespace RectangleArea
         static void Main(string[] args)
         {
             // In the next two rows, we get the length of the sides "a" and "b" of the rectangle from the console
-            double a = double.Parse(Console.ReadLine());
-            double b = double.Parse(Console.ReadLine());
+            double sideA = double.Parse(Console.ReadLine());
+            double sideB = double.Parse(Console.ReadLine());
 
             // We calculate area of rectangle.
-            var sum = a * b;
+            var rectangleArea = sideA * sideB;
 
             // We print to the console the result rounded up to two marks after the decimal point.
-            Console.WriteLine("{0:f2}",sum);
+            Console.WriteLine("{0:f2}",rectangleArea);
            
         }
     }
