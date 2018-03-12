31114bebbcb98f264ba6a4508781706d92d521cf
ryan8811 <todor_papanov@abv.bg>
2018-03-12 21:52:49 +0000

    Changed variable's names on Problem 2

---
+++ intro-and-basic-syntax/exercise/Problem 2/Program.cs
@@ -0,0 +1,23 @@
+﻿//CONFIRMED from s-tdrv
+
+using System;
+
+namespace RectangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // In the next two rows, we get the length of the sides "a" and "b" of the rectangle from the console
+            double wight = double.Parse(Console.ReadLine());
+            double hight = double.Parse(Console.ReadLine());
+
+            // We calculate area of rectangle.
+            var area = wight * hight;
+
+            // We print to the console the result rounded up to two marks after the decimal point.
+            Console.WriteLine("{0:f2}",area);
+      
+        }
+    }
+}
